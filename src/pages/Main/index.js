af57eea5ceb4d88a1852b13bb35ce2d9d520c0c4
Alexandre Almeida Vessoni <av.vessoni@gmail.com>
2019-08-30 12:12:34 -0300

    Add style in main, but need maintenance

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
-  return <Title>Hello</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositorio" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
