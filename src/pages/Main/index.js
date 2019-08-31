0305ca60239fd17c668d5bde16d17d95bc2d47be
= <av.vessoni@gmail.com>
2019-08-31 15:22:44 -0300

    Add value in localStorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
+import { stringify } from 'querystring';
 import api from '../../services/api';
 
 import { Container, Form, SubmitButton, List } from './styles';
@@ -11,6 +12,22 @@ export default class Main extends Component {
     repositories: [],
     loading: false,
   };
+
+  // Load the data localstorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // save data localstorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
 
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
